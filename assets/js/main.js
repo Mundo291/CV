b686e30346e0c9cc94f66126a6d5ac87b67e037c
Mundo291 <65348951+Mundo291@users.noreply.github.com>
2020-11-02 02:22:57 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/**
-* Template Name: iPortfolio - v2.0.1
-* Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 !(function($) {
   "use strict";
 
@@ -191,3 +185,31 @@
   });
 
 })(jQuery);
+const lightMode = document.querySelector('.sun');
+const darkMode = document.querySelector('.moon');
+
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+lightMode.onclick = function(){
+  document.documentElement.setAttribute('data-theme', 'dark');
+  lightMode.style = "display: none";
+  darkMode.style = "display: block";
+  localStorage.setItem('theme', 'dark');
+}
+
+darkMode.onclick = function(){
+  document.documentElement.setAttribute('data-theme', 'light');
+  lightMode.style = "display: block";
+  darkMode.style = "display: none";
+  localStorage.setItem('theme', 'light');
+  
+}
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        lightMode.style = "display: none";
+      darkMode.style = "display: block";
+    }
+}
